5618ef33dd138664364b9f2afe28cba8a35353b8
Sensei_P <padraigo.donoghue@gmail.comgit config --global user.name Sensei_Pgit config --global user.email padraigo.donoghue@gmail.com>
2023-11-16 23:18:08 +0000

    Refactored Fetch to Component

---
+++ src/app/components/Bloglist.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { MdNightlightRound, MdWbSunny } from "react-icons/md";
+
+const fetchBlogs = async () => {
+  const reqOptions = {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+    },
+  };
+  // const request = await fetch(`${config.api}/api/blogs?populate=*`, reqOptions)
+  const request = await fetch(
+    `https://strapi-production-8b2a.up.railway.app/api/blogs?populate=*`,
+    reqOptions
+  );
+
+  // api/blogs?populate=*`
+  const response = await request.json();
+
+  console.log(process.env.STRAPI_TOKEN);
+  console.log(response, "response");
+
+  return response;
+};
+
+// {blogs,data.map(blog => (
+//     <Link href={blog.attributes.slug} key={blog.id}>{blog.attributes.blog}</Link>
+// )
+const Header = async () => {
+  const blogs = await fetchBlogs();
+  console.log("blogs", blogs);
+  return (
+    <>
+      <p>test</p>
+      {blogs.data.map((blog) => (
+        <Link href={blog.attributes.slug} key={blog.id}>
+          {blog.attributes.blog}
+        </Link>
+      ))}
+    </>
+  );
+};
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,19 @@ import config from '../config';
 import BlogDetails from "./[slug]/page";
 import Link from 'next/link';
 import Header from './components/Header';
+import Bloglist from './components/Bloglist';
 // import fetchBlogs from "./helpers/fetch-blogs";
 
-const fetchBlogs = async () => {
-  const reqOptions = {
-    headers: {
-      Authorization: `Bearer ${process.env.STRAPI_TOKEN}` 
-    }
-  }
-  // const request = await fetch(`${config.api}/api/blogs?populate=*`, reqOptions)
-  const request = await fetch(`https://strapi-production-8b2a.up.railway.app/api/blogs?populate=*`, reqOptions)
 
-  // api/blogs?populate=*`
-  const response = await request.json();
-  
-  console.log(process.env.STRAPI_TOKEN)
-  console.log(response,'response')
-
-
-  return response
-
-}
 
 
 const Home = async () => {
-  const blogs = await fetchBlogs();
-  console.log('blogs', blogs)
+  
   
   return (
     <>
     <Header />
-    {blogs.data.map(blog => (
-      <Link href={blog.attributes.slug} key={blog.id}>{blog.attributes.blog}</Link>
-    ))}
+    <Bloglist />
     </>
   )
 }
